833f252d9f673a7c39628a3ea0afa46e5d1eb83a
Ariel Diaz <arielemildiaz12@gmail.com>
2026-02-18 16:44:08 -0400

    Agregue un atributo Bool a tarea, para saber su estado y asi poder eliminarlas y ver su estado en el menu

--- GestorDeTareas.hs
+++ GestorDeTareas.hs
@@ -2,19 +2,21 @@ module GestorDeTareas where
 
 import System.IO
 
--- Sebastián Almánzar #10154600
+-- Sebastián Almánzar 10154600
+-- Ariel Diaz 10153927
+
 -- 1. Tipo de Dato a presentar
-data Tarea = Tarea Int String
+data Tarea = Tarea Int String Bool
 
 -- 2. Funciones Puras (clave del programa, se muestra tarea, se obtiene su ID, se agrega tarea, eliminar tarea por su ID.
 
 -- La tarea pasa de un ID (Int) a un String
 visualizarTarea :: Tarea -> String
-visualizarTarea (Tarea id desc) = "ID: " ++ show id ++ " - " ++ desc
+visualizarTarea (Tarea id desc comp) = "ID: " ++ show id ++ " - " ++ desc ++ " - " ++ show comp
 
 -- Se obtiene un ID
 obtenerIdDeTarea :: Tarea -> Int
-obtenerIdDeTarea (Tarea id _) = id
+obtenerIdDeTarea (Tarea id _ _) = id
 
 -- Se genera un nuevo ID
 siguienteId :: [Tarea] -> Int
@@ -25,8 +27,19 @@ siguienteId tareas = obtenerIdDeTarea (last tareas) + 1
 agregarTarea :: [Tarea] -> String -> [Tarea]
 agregarTarea tareas descripcion =
   let nuevoID = siguienteId tareas
-      nuevaTarea = Tarea nuevoID descripcion
+      nuevaTarea = Tarea nuevoID descripcion False
    in tareas ++ [nuevaTarea]
+
+-- Cambiar el Bool de una tarea a true
+completarTarea :: [Tarea] -> Int -> [Tarea]
+completarTarea tareas idTarget =
+  map
+    ( \(Tarea id desc comp) ->
+        if id == idTarget
+          then Tarea id desc True
+          else Tarea id desc comp
+    )
+    tareas
 
 -- Eliminar una tarea por su ID
 eliminarTarea :: [Tarea] -> Int -> [Tarea]
@@ -34,15 +47,48 @@ eliminarTarea tareas idEliminar =
   filter (\t -> obtenerIdDeTarea t /= idEliminar) tareas
 
 main :: IO ()
-main = do
-  let listaInicial = []
-  let lista1 = agregarTarea listaInicial "Tengo que realizar Practica-3 de Programacion Web"
-  let lista2 = agregarTarea lista1 "Pagar la luz"
-  let lista3 = agregarTarea lista2 "Estudiar Haskell"
+main = menu []
 
-  putStrLn "-----Mis Tareas -----"
-  mapM_ (putStrLn . visualizarTarea) lista2
+menu :: [Tarea] -> IO ()
+menu tareas = do
+  putStrLn "\n========== GESTOR DE TAREAS =========="
+  -- Mostramos la lista actual
+  if null tareas
+    then putStrLn "[ Lista vacia ]"
+    else mapM_ (putStrLn . visualizarTarea) tareas
 
-  putStrLn "\n--- Eliminando Tarea 1 ---"
-  let listaFinal = eliminarTarea lista2 1
-  mapM_ (putStrLn . visualizarTarea) listaFinal
+  putStrLn "\n1. Agregar Tarea"
+  putStrLn "2. Marcar como Completada"
+  putStrLn "3. Ver solo Pendientes"
+  putStrLn "4. Eliminar Tarea"
+  putStrLn "5. Salir"
+  putStr "Seleccione una opción: "
+  hFlush stdout
+  opcion <- getLine
+  case opcion of
+    "1" -> do
+      putStr "Descripción de la tarea: "
+      desc <- getLine
+      let nuevasTareas = agregarTarea tareas desc
+      menu nuevasTareas -- Llamada con la lista actualizada
+    "2" -> do
+      putStr "ID de la tarea a completar: "
+      idStr <- getLine
+      let idInt = read idStr -- Convertimos texto a número
+      menu (completarTarea tareas idInt)
+    "3" -> do
+      putStrLn "\n--- Tareas Pendientes ---"
+      let pendientes = filter (\(Tarea _ _ comp) -> not comp) tareas
+      mapM_ (putStrLn . visualizarTarea) pendientes
+      putStrLn "Presione Enter para volver..."
+      _ <- getLine
+      menu tareas -- Volvemos al menú con todas las tareas
+    "4" -> do
+      putStr "ID de la tarea a eliminar: "
+      idStr <- getLine
+      let idInt = read idStr
+      menu (eliminarTarea tareas idInt)
+    "5" -> putStrLn "Cerrando programa"
+    _ -> do
+      putStrLn "Opción no válida"
+      menu tareas
